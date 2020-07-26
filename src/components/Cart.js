18f1ad5699254895ce5d5139401e2f58306a82da
Felipe Vieira <felipe_brito@id.uff.br>
2020-07-26 00:36:45 -0300

    Finalização do design do carrinho

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -22,40 +22,45 @@ class Cart extends Component{
             (  
                 this.props.items.map(item=>{
                     return(
-                        <div class="flex justify-center my-6">
-                        <div class="flex flex-col w-full p-8 text-gray-800 bg-white shadow-lg pin-r pin-y md:w-4/5 lg:w-4/5">
+                      
+                      <div class="flex justify-center my-6">
+                <div class="flex flex-col w-3/4 p-8 text-gray-800 bg-white shadow-lg pin-r pin-y mr-8 ml-12">
+                    
                           <div class="flex-1">
-                            <table class="w-full text-sm lg:text-base" cellspacing="0">
+                            <table class="w-full table-auto text-sm lg:text-base" cellspacing="0">
                               <thead>
-                                <tr class="h-12 uppercase">
+                                <tr class="h-12">
+                                  <th class="text-left font-sans my-4 text-4xl font-semibold dark:text-gray-400">Carrinho</th>
+                                </tr>
+                                <tr class="h-12 uppercase font-sans">
                                   <th class="hidden md:table-cell"></th>
-                                  <th class="text-left">Product</th>
-                                  <th class="lg:text-right text-left pl-5 lg:pl-0">
-                                    <span class="lg:hidden" title="Quantity">Qtd</span>
-                                    <span class="hidden lg:inline">Quantity</span>
+                                  <th class="text-center font-sans">Nome</th>
+                                  <th class="text-center pl-5 lg:pl-0">
+                                    <span class="lg:hidden" title="Quantidade">Qtd</span>
+                                    <span class="hidden lg:inline">Quantidade</span>
                                   </th>
-                                  <th class="hidden text-right md:table-cell">Unit price</th>
-                                  <th class="text-right">Total price</th>
+                                  <th class="hidden text-center md:table-cell">Preço unitário</th>
+                                  <th class="text-center">Preço total</th>
                                 </tr>
                               </thead>
                               <tbody>
                                 <tr>
-                                  <td class="hidden pb-4 md:table-cell">
-                                    <a href="#">
+                                  <td class="w-10 pl-10">
+                                    <a href="#" >
                                       <img src="https://limg.app/i/Calm-Cormorant-Catholic-Pinball-Blaster-yM4oub.jpeg" class="w-20 rounded" alt="Thumbnail"></img>
                                     </a>
                                   </td>
                                   <td>
-                                    <a href="#">
-                                      <p class="mb-2 md:ml-4">Earphone</p>
+                                    <a href="#" class="text-center">
+                                      <p>Earphone</p>
                                       <form action="" method="POST">
-                                        <button type="submit" class="text-gray-700 md:ml-4">
-                                          <small>(Remove item)</small>
+                                        <button type="submit" class="text-gray-700 ">
+                                          <small>(Remover item)</small>
                                         </button>
                                       </form>
                                     </a>
                                   </td>
-                                  <td class="justify-center md:justify-end md:flex mt-6">
+                                  <td class="justify-center md:flex mt-6">
                                     <div class="w-20 h-10">
                                       <div class="relative flex flex-row w-full h-8">
                                       <input type="number" value="2" 
@@ -63,89 +68,57 @@ class Cart extends Component{
                                       </div>
                                     </div>
                                   </td>
-                                  <td class="hidden text-right md:table-cell">
-                                    <span class="text-sm lg:text-base font-medium">
+                                  <td class="text-center">
+                                    <span class="font-sans text-xl ">
                                       10.00€
                                     </span>
                                   </td>
-                                  <td class="text-right">
-                                    <span class="text-sm lg:text-base font-medium">
+                                  <td class="text-center">
+                                    <span class="font-sans text-xl ">
                                       20.00€
                                     </span>
                                   </td>
                                 </tr> 
-                                <tr>
-                                  <td class="hidden pb-4 md:table-cell">
-                                    <a href="#">
-                                      <img src="https://limg.app/i/Cute-Constrictor-Super-Sexy-Military-Enforcer-W7mvBp.png" class="w-20 rounded" alt="Thumbnail" />
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <p class="mb-2 md:ml-4">Tesla Model 3</p>
-                                    <form action="" method="POST">
-                                      <button type="submit" class="text-gray-700 md:ml-4">
-                                        <small>(Remove item)</small>
-                                      </button>
-                                    </form>
-                                  </td>
-                                  <td class="justify-center md:justify-end md:flex md:mt-4">
-                                  <div class="w-20 h-10">
-                                    <div class="relative flex flex-row w-full h-8">
-                                    <input type="number" value="3" 
-                                      class="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black" />
-                                    </div>
-                                  </div>
-                                  </td>
-                                  <td class="hidden text-right md:table-cell">
-                                    <span class="text-sm lg:text-base font-medium">
-                                      49,600.01€
-                                    </span>
-                                  </td>
-                                  <td class="text-right">
-                                    <span class="text-sm lg:text-base font-medium">
-                                      148,800.03€
-                                    </span>
-                                  </td>
-                                </tr> 
-                                <tr>
-                                  <td class="hidden pb-4 md:table-cell">
-                                    <a href="#">
-                                      <img src="https://limg.app/i/Successful-Spider-Biblical-Mutant---Total-War-lKoE7D.jpeg" class="w-20 rounded" alt="Thumbnail"/>
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <p class="mb-2 md:ml-4">Bic 4 colour pen</p>
-                                    <form action="" method="POST">
-                                      <button type="submit" class="text-gray-700 md:ml-4">
-                                        <small>(Remove item)</small>
-                                      </button>
-                                    </form>
-                                  </td>
-                                  <td class="justify-center md:justify-end md:flex md:mt-8">
-                                  <div class="w-20 h-10">
-                                    <div class="relative flex flex-row w-full h-8">
-                                    <input type="number" value="5" 
-                                      class="w-full font-semibold text-center text-gray-700 bg-gray-200 outline-none focus:outline-none hover:text-black focus:text-black" />
-                                    </div>
-                                  </div>
-                      
-                                  </td>
-                                  <td class="hidden text-right md:table-cell">
-                                    <span class="text-sm lg:text-base font-medium">
-                                      1.50€
-                                    </span>
-                                  </td>
-                                  <td class="text-right">
-                                    <span class="text-sm lg:text-base font-medium">
-                                      7.50€
-                                    </span>
-                                  </td>
-                                </tr> 
+                                
                               </tbody>
                             </table>
                           </div>
+                </div>
+                <div class="rounded-b border-t-0 z-10 w-1/4 mr-12">
+                    <div class="shadow-xl w-full">
+
+                    <div class="p-2 flex bg-pink-200 cursor-pointer border-b border-gray-100 rounded-t"  >
+                            <div class="flex-auto text-sm w-32">
+                                <div class="font-sans text-2xl text-center text-gray-900 font-bold">Carrinho</div>
+                            </div>
                         </div>
-                      </div>         
+                        <div class="p-2 flex bg-pink-200 cursor-pointer border-b border-gray-100 px-12"  >
+                            <div class="flex-auto text-sm w-32">
+                                <div class="font-bold">2 produtos</div>
+                            </div>
+                            <div class="flex flex-col w-18 font-medium items-end">
+                                $12.22</div>
+                        </div>
+                        
+                        <div class="p-2 flex bg-pink-300 cursor-pointer border-b border-gray-100 px-12"  >
+                            <div class="flex-auto text-sm w-32">
+                                <div class="font-bold text-xl">Total</div>
+                            </div>
+                            <div class="flex flex-col w-18 font-bold text-2xl items-end">
+                                $12.22</div>
+                        </div>
+
+                        <div class="bg-pink-200 p-4 justify-center flex">
+                            <button class="text-base  undefined  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+                            hover:bg-pink-700 hover:text-teal-100 
+                            bg-pink-100 
+                            text-pink-700 
+                            border duration-200 ease-in-out 
+                            border-pink-600 transition">Finalizar compra</button>
+                        </div>
+                    </div>
+                 </div>
+        </div>      
                     )
                 })
             ):
@@ -167,11 +140,12 @@ class Cart extends Component{
                             </tr>
                         </thead>
                         </table>
-                    </div>
+                    </div> 
                 </div>
                 <div class="rounded-b border-t-0 z-10 w-1/4 mr-12">
                     <div class="shadow-xl w-full">
-                        
+
+                               
                         <div class="p-2 flex bg-pink-200 cursor-pointer border-b border-gray-100 rounded-t"  >
                             <div class="flex-auto text-sm w-32">
                                 <div class="font-sans text-2xl text-center text-gray-900 font-bold">Resumo do carrinho</div>
@@ -188,9 +162,9 @@ class Cart extends Component{
                             text-gray-500 
                             border duration-200 ease-in-out 
                             border-gray-500 transition">Finalizar compra</button>
-                        </div>
+                        </div> 
                     </div>
-            </div>
+                 </div>
         </div> 
             
        )
